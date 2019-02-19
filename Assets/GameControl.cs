1d206e4b23508f807427c060609a05830fe44a8c
Zion Perez <zionperez@gmail.com>
2019-02-19 10:58:38 +0900

    Complete Flappy Bird Style lesson 6/10.

---
+++ Assets/GameControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            print("Error: another GameControl object found.");
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
